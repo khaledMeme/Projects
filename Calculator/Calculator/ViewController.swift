e0c9ea56a9d3faaca3820c0e36ae7b8fde487ede
khaledMeme <khaled.meme97@gmail.com>
2020-01-28 02:08:36 +0200

    calculator

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Khaled Meme on 1/28/20.
+//  Copyright © 2020 Khaled Meme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
