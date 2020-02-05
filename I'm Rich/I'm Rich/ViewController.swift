00d93f7b8196503f07f5a48cf1c7f699ecaba3a0
khaledMeme <khaled.meme97@gmail.com>
2020-02-05 04:18:32 +0200

    I'm Rich

---
+++ I'm Rich/I'm Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I'm Rich
+//
+//  Created by Khaled Meme on 11/23/19.
+//  Copyright © 2019 Khaled Meme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
